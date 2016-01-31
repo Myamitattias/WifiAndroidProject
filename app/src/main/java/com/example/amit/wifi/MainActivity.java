c10ec81624cee9b0111bade47cb1ec5edb901aec
Myamitattias <amitattias20@gmail.com>
2016-01-31 09:51:03 +0200

    LISTVIEW dot work

--- app/src/main/java/com/example/amit/wifi/MainActivity.java
+++ app/src/main/java/com/example/amit/wifi/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.amit.wifi;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,30 +10,41 @@ import android.widget.Button;
 import android.view.View;
 import android.net.wifi.*;
 import android.widget.CompoundButton;
+import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-private static final String TAG = "wifi";
+    //region CONSTANTS
+
+    private static final String TAG = "wifi";
     TextView text ;
     Button scan;
     WifiManager wifi;
+    TextView strong;
     BroadcastReceiver receiver;
+    ListView wifis;
+    WifiInfo info;
+    //endregion
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
         scan = (Button) findViewById(R.id.buttonScan);
         text = (TextView) findViewById(R.id.textView);
+        strong = (TextView) findViewById(R.id.strong);
+        wifis = (ListView) findViewById(R.id.wifis);
         final Switch wifiSwich = (Switch) findViewById(R.id.wifibutton);
+        info = wifi.getConnectionInfo();
+        text.append("\n\n wifi :" + info.toString());
         scan.setOnClickListener(this);
         wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        WifiInfo info = wifi.getConnectionInfo();
-        text.append("\n\n wifi :" + info.toString());
 
         List<WifiConfiguration> configurations = wifi.getConfiguredNetworks();
         for (WifiConfiguration configuration : configurations) {
@@ -46,6 +56,8 @@ private static final String TAG = "wifi";
         registerReceiver(receiver, new IntentFilter(
                 WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
         Log.d(TAG, "onCreate");
+
+    //region  WIFI SWITCH
         if(wifi.isWifiEnabled() == true)
             wifiSwich.setChecked(true);
         else
@@ -63,6 +75,7 @@ private static final String TAG = "wifi";
 
             }
         });
+        //endregion
     }
 
     @Override
--- app/src/main/java/com/example/amit/wifi/WifiScaner.java
+++ app/src/main/java/com/example/amit/wifi/WifiScaner.java
@@ -7,7 +7,7 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.util.Log;
 import android.widget.Toast;
-
+import java.lang.String;
 import java.util.List;
 
 /**
@@ -23,15 +23,21 @@ public class WifiScaner extends BroadcastReceiver {
     }
     @Override
     public void onReceive(Context arg0 , Intent arg1){
-        List<ScanResult>results = main.wifi.getScanResults();
+        List<ScanResult>results =main.wifi.getScanResults();
+        final int endlist = results.size();
+        String[] wifilist = new String[endlist];
         ScanResult best = null;
+        for (int i = 0;i<endlist;i++) {
+            wifilist[i] = results.get(i).SSID;
+        }
         for (ScanResult result : results){
-            if(best == null || WifiManager.calculateSignalLevel(best.level , result.level)<0)
+            if(best == null || WifiManager.calculateSignalLevel(best.level, result.level)<0)
                 best = result;
         }
         String message = String.format("%s network found.%s is the strongest" ,results.size(),best.SSID);
         Toast.makeText(main ,message,0).show();
-        Log.d(TAG,"onReceive() message : "+message );
+        main.strong.setText(message);
+        Log.d(TAG, "onReceive() message : " + message);
 
     }
 
